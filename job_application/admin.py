59760f8d71d2950e615073f71b2ff92ac1dea4d9
MAB-100 <shahabbas661@gmail.com>
2024-08-02 19:12:29 +0500

    implemented admin inteface and nav bar

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+class FormAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email", "date", "occupation"]  # Display these fields in the admin site
+    search_fields = ["first_name", "last_name", "email"]  # Add a search bar to the admin site
+    list_filter = ["date", "occupation"]  # Add filters to the admin site
+    ordering = ["-date"]  # Order the forms by date in descending order
+    
+
+
+admin.site.register(Form, FormAdmin)  # Register the Form model with the admin site
+
